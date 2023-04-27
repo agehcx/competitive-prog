a4c393b36d591a43e568624dff9e508a7a856505
bfial <45969692+uwuflex@users.noreply.github.com>
2023-04-28 00:39:22 +0700

    Create 716.py

---
+++ Devlab/716.py
@@ -0,0 +1,4 @@
+https://www.borntodev.com/devlab/task/716
+for i in range(int(input())):
+    list = input().split(", ")
+    print( ( (-1)**(len(list)+1)) * (int(list[0])) * (int(list[len(list)-1] )))
