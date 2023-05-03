dbd2c71b6e356820d1db06f5512282dbb541a554
bfial <45969692+uwuflex@users.noreply.github.com>
2023-05-04 00:57:18 +0700

    4/5/2023

---
+++ Leetcode/551_Student_Attendance_Record_I.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int totalAbsent = 0;
+        int ConsecutiveLate = 0;
+        for(char idx : s) {
+            if(idx=='A') {
+                totalAbsent++; 
+                ConsecutiveLate = 0;
+            }
+            else if(idx=='L') {
+                ConsecutiveLate++;
+            }
+            else ConsecutiveLate = 0;
+            if(totalAbsent>1 || ConsecutiveLate>2) return false;
+        }
+        return true;
+    }
+};
