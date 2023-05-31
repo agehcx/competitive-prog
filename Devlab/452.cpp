932e42d397d8a64e3dff4d1c15568924afb81cb7
bfial <45969692+uwuflex@users.noreply.github.com>
2023-05-31 19:18:44 +0700

    Update 452.cpp

--- Devlab/452.cpp
+++ Devlab/452.cpp
@@ -5,28 +5,28 @@ using namespace std;
 
 int main()
 {
-	string x;
+    string x;
     getline(cin,x);
     int N;
     for(int i=0; i<x.size(); i++) {
-      if(x[i] >= '0' && x[i] <= '9') {
-        N = N * 10 + x[i] - '0';
-      }
+        if(x[i] >= '0' && x[i] <= '9') {
+            N = N * 10 + x[i] - '0';
+        }
     }
     int arr[N]; 
     for(int i=0; i<N; i++) {
-      cin>>arr[i];
+        cin>>arr[i];
     }
     for(int i=1; i<=N; i++) {
-         priority_queue<int,vector<int>,greater<int>> pq;
-         for(int j=0; j<i; j++) {
-           pq.push(arr[j]);
+        priority_queue<int,vector<int>,greater<int>> pq;
+        for(int j=0; j<i; j++) {
+            pq.push(arr[j]);
         } 
         cout<<"ROW "<<i<<" : [";
         while(!pq.empty()) {
-          cout<<pq.top();
-          if(pq.size()!=1) cout<<", ";
-          pq.pop();
+            cout<<pq.top();
+            if(pq.size()!=1) cout<<", ";
+            pq.pop();
         }
         cout<<"]\n";
     }
