1dadf11837a537e1c2cdbd580b0d403a2a929225
bfial <45969692+uwuflex@users.noreply.github.com>
2023-05-31 19:15:27 +0700

    Update 850.cpp

--- Devlab/850.cpp
+++ Devlab/850.cpp
@@ -11,27 +11,28 @@ int main()
     cin >> x;
     double avg = 0;
     for(int i=1; i<=x; i++) {
-      string id;
-      double score;
-      cin>>id>>score;
-      pq.push({score,id});
-      if(score>100) {
-        cout<<"Error";
-        return 0;
-      }
-      avg += score;
+        string id;
+        double score;
+        cin>>id>>score;
+        pq.push({score,id});
+        if(score>100) {
+            cout<<"Error";
+            return 0;
+        }
+        avg += score;
     }
     avg = avg / x;
     if(pq.top().first > 100 ) cout<<"Error";
     while(!pq.empty()) {
-      if(pq.top().first >= avg ) {
-        int id = stoi(pq.top().second);
-        pq2.push(id);
-      }
-      pq.pop();
+        if(pq.top().first >= avg ) {
+            int id = stoi(pq.top().second);
+            pq2.push(id);
+        }
+        pq.pop();
     } 
     while(!pq2.empty()) {
       cout<<pq2.top()<<'\n';
       pq2.pop();
     } 
 }
+
