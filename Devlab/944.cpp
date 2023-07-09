51edcda82bd2105a3612c961f07593554ead2188
ajc <45969692+agehcx@users.noreply.github.com>
2023-07-10 00:43:06 +0700

    O(Q)

--- Devlab/944.cpp
+++ Devlab/944.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 const int MAXN = 1e6 + 5;
 
+vector<int> dp(MAXN, 0);
 vector<bool> prime(MAXN, true);
 vector<bool> abundant(MAXN, false);
 
@@ -19,7 +20,7 @@ bool isAbundantNumber(int Number) {
     return sumDivisor > Number;
 }
 
-void precompute() {
+void preCompute() {
     for (int i = 2; i <= MAXN; i++) {
 		if (!prime[i] && isAbundantNumber(i)) {
             abundant[i] = true;
@@ -39,28 +40,29 @@ void sieveOfEratosthenes() {
     }
 }
 
+void prefixSum() {
+    for (int i = 2; i <= MAXN; i++) {
+      dp[i] = dp[i-1] + abundant[i];
+    }
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 	
     sieveOfEratosthenes();
-	precompute();
+preCompute();
+    prefixSum();
 	
     int Q;
     cin >> Q;
 
     while (Q--) {
-    	
+      
         int count = 0;
         int L, R;
         cin >> L >> R;
 
-        for (int i = L; i <= R; i++) {
-            if (abundant[i]) count++;
-        }
-
-        cout << count << '\n';
+        cout << dp[R] - dp[L-1] << '\n';
     }
-	
-    // Time complexity : O(Q * R)
 }
